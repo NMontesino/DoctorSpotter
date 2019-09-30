be1c86134172143104d4d86d91705c436083dc3b
github username <github email address>
2019-09-30 16:51:51 -0400

    Signup page created and password confirmation set

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,2 +1,35 @@
 class PatientsController < ApplicationController
+
+
+  def new
+    @patient = Patient.new
+  end
+
+  def create
+
+    if password_confirmation?
+      @patient = Patient.create(set_patient_params)
+      redirect_to patient_path(@patient)
+    else
+      flash[:error] = "Passwords don't match. Please, try it again"
+      redirect_to new_patient_path
+    end
+
+  end
+
+
+  private
+  def set_patient_params
+    params.require(:patient).permit(:first_name, :last_name, :email, :password)
+  end
+
+  def password_confirmation?
+    if params[:patient][:password] == params[:patient][:password_confirmation]
+      return true
+    else
+      return false
+    end
+  end
+
+
 end
--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,4 +1,8 @@
 class Patient < ApplicationRecord
     has_many :appointments
     has_many :doctors, through: :appointments
+    has_secure_password
+
+    validates :email, uniqueness: true
+    validates :password_digest, presence: true
 end
